311a5187740b873cfd322e8e73331c20e4bce6ab
Alfredo Gabriel <amgabriel1@hotmail.com>
2022-11-04 17:21:46 -0300

    fix: conexão com o banco e dependencias

--- src/main/java/com/wassabi/App.java
+++ src/main/java/com/wassabi/App.java
@@ -1,14 +1,28 @@
 package com.wassabi;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.wassabi.model.Cliente;
+import com.wassabi.model.Endereco;
+
 /*
  * Está e classe principal que sera chamada para iniciar a aplicação
  */
 public class App {
-    
+    private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("WassabiApp");
+
     /** 
      * @param args
      */
     public static void main(String[] args) {
         
     }
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    } 
+    
 }
